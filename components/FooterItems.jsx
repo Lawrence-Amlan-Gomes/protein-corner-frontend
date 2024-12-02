7ddcb96f5d2ddce186db7f11968656f6fc8dfa52
Lawrence-Amlan-Gomes <amlangomes@gmail.com>
2024-12-02 15:14:55 +0600

    footeritem and useCategory updated

--- components/FooterItems.jsx
+++ components/FooterItems.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-export default function ({ name, linkTo }) {
+export default function FooterItem({ name, linkTo }) {
   return (
     <Link href={linkTo}>
       <div className="w-full flex justify-center items-center text-center lg:mb-5 mb-5 lg:text-[16px] text-[14px]">
